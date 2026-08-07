b73bca9ad00bae0766c1a007ed347a656649ef45
liuw1535 <liuw15935@gmail.com>
2026-08-07 12:37:56 +0800

    增加构建脚本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,21 +18,22 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
         }
         configureEach {
             //应用名
             //app name
-            resValue("string","app_name","XXX Renderer")
+            resValue("string","app_name","SimpleFPE Renderer")
             //包名后缀
             //package name Suffix
-            applicationIdSuffix = ".xxx"
+            applicationIdSuffix = ".simplefpe"
 
             //渲染器在启动器内显示的名称
             //The name displayed by the renderer in the launcher
-            manifestPlaceholders["des"] = ""
+            manifestPlaceholders["des"] = "SimpleFPE"
             //渲染器的具体定义 格式为 名称:渲染器库名:EGL库名 例如 LTW:libltw.so:libltw.so
             //The specific definition format of a renderer is ${name}:${renderer library name}:${EGL library name}, for example:   LTW:libltw.so:libltw.so
-            manifestPlaceholders["renderer"] = ""
+            manifestPlaceholders["renderer"] = "SimpleFPE:libSimpleFPEWrapper.so:libSimpleFPEWrapper.so"
 
             //特殊Env
             //Special Env
